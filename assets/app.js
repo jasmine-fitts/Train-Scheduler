33b91ba30de968adc01ce6da50d8edea7ba0c927
Jasmine Fitts <jasminefitts@jasmines-mbp.attlocal.net>
2018-06-04 18:26:41 -0400

    updated js and index

--- assets/app.js
+++ assets/app.js
@@ -1,55 +1,34 @@
 // Initialize Firebase
 var config = {
-     apiKey: "AIzaSyDm7iLTMVXRp-gZjl0CeIleVMmT2XKUe2I",
-     authDomain: "train-scheduler-8f1d4.firebaseapp.com",
-     databaseURL: "https://train-scheduler-8f1d4.firebaseio.com",
-     projectId: "train-scheduler-8f1d4",
-     storageBucket: "train-scheduler-8f1d4.appspot.com",
-     messagingSenderId: "1033840179244"
-   };
-   firebase.initializeApp(config);
+    apiKey: "AIzaSyD4q2A9pB5yR0ieWWRlSI9WS_c_BJe2wnc",
+    authDomain: "train-scheduler2-5787b.firebaseapp.com",
+    databaseURL: "https://train-scheduler2-5787b.firebaseio.com",
+    projectId: "train-scheduler2-5787b",
+    storageBucket: "",
+    messagingSenderId: "858088627198"
+  };
+  firebase.initializeApp(config);
 
    var database = firebase.database();
    console.log(firebase)
 
-// Submit button for adding a new train
-  $("form").on("submit", function(event){
-    event.preventDefault();
+    $("#send").click(function(){
+        event.preventDefault();
 
     var name = $("#train-name").val().trim();
     var destination = $("#train-destination").val().trim();
     var time = $("#train-time").val().trim();
     var frequency = $("#train-frequency").val().trim();
 
-    console.log(name);
-    console.log(destination);
-    console.log(time);
-    console.log(frequency);
+   var newTrain = {
+       name: name,
+       destination: destination,
+       time: time, 
+       frequency: frequency, 
+   
+   };
 
-//Created local object for holding train data
-    var newTrain = {
-        train: name,
-        destination: destination,
-        time: time, 
-        frequency: frequency,
-        
-    
-    };
-
-    
-
-    //Code for setting values in the database//
-
-    // database.ref().on("child_added", (snap) (newTrain);
-
-    // // console.log(newTrain.name);
-    // (newTrain.destionation);
-    // (newTrain.time);
-    // (newTrain.frequency);
-        
-     
-  });
-
-  //Code to app to calculate when the next train will arrive  --- this should be relative until the current time
-
-  //
+   console.log("hi")
+   console.log(newTrain);
+   database.ref().push(newTrain).key;
+    })
